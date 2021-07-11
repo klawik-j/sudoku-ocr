3e9ca3544ddfa93fdbee030710792937120e4fd0
klawik-j <kklawik345@gmail.com>
2021-07-11 02:15:33 +0200

    add: find cells coordinates

--- code/ocr.py
+++ code/ocr.py
@@ -1,15 +1,17 @@
+from typing import List
+
 from imutils.perspective import four_point_transform
 from imutils import grab_contours, resize
 from skimage.segmentation import clear_border
 from numpy import ndarray
 import cv2
 
-WAIT_TIME = 5000
+WAIT_TIME = 1000
 
 
 class Board:
-    """Class of a sudoku board
-    """
+    """Class of a sudoku board"""
+
     def __init__(self, img_path):
         self._img_path = img_path
         self.original_img = self.load_img(self._img_path)
@@ -21,6 +23,7 @@ class Board:
             self.original_img,
             self.board_contours,
         )
+        self.cells_cords = self.find_cells(self.board)
 
     @staticmethod
     def load_img(img_path: str) -> ndarray:
@@ -122,21 +125,57 @@ class Board:
         )
         return board
 
-    def print_img(self):
-        """Prints original image
+    @staticmethod
+    def find_cells(board: ndarray) -> List:
+        """Looks for cells coordinates
+
+        Args:
+            board (ndarray): image of board
+
+        Returns:
+            List: list of coordinates
+
+        +---+---+---+---+---+---+---+---+---+
+        | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+        +---+---+---+---+---+---+---+---+---+
+        | 9 | 10| 11| 12| 13| 14| 15| 16| 17|
+        +---+---+---+---+---+---+---+---+---+
+        .
+        .
+        .
+        +---+---+---+---+---+---+---+---+---+
+        | 80| 81| 82| 83| 84| 85| 86| 87| 88|
+        +---+---+---+---+---+---+---+---+---+
         """
+        cell_width = board.shape[1] // 9
+        cell_height = board.shape[0] // 9
+
+        cells_cords = []
+
+        for y in range(0, 9):
+            for x in range(0, 9):
+                cell_x_start = x * cell_width
+                cell_x_end = (x + 1) * cell_width
+                cell_y_start = y * cell_height
+                cell_y_end = (y + 1) * cell_height
+
+                cell = (cell_x_start, cell_x_end, cell_y_start, cell_y_end)
+                cells_cords.append(cell)
+
+        return cells_cords
+
+    def print_img(self):
+        """Prints original image"""
         cv2.imshow("original image", self.original_img)
         cv2.waitKey(WAIT_TIME)
 
     def print_thresh_board(self):
-        """Prints treshold on image
-        """
+        """Prints treshold on image"""
         cv2.imshow("board after thresholding", self.board_thresh)
         cv2.waitKey(WAIT_TIME)
 
     def print_board_contours(self):
-        """Prints image with board contours marked
-        """
+        """Prints image with board contours marked"""
         board_on_img = self.original_img
         cv2.drawContours(
             board_on_img,
@@ -149,10 +188,21 @@ class Board:
         cv2.waitKey(WAIT_TIME)
 
     def print_board(self):
-        """Prints only board
-        """
+        """Prints only board"""
         cv2.imshow("Board", self.board)
         cv2.waitKey(WAIT_TIME)
+
+    def print_cells(self):
+        """Print cells one by one"""
+        for cell in self.cells_cords:
+            cv2.imshow(
+                "cells",
+                self.board[
+                    cell[2] : cell[3],
+                    cell[0] : cell[1],
+                ],
+            )
+            cv2.waitKey(WAIT_TIME)
 
 
 if __name__ == "__main__":
@@ -164,5 +214,7 @@ if __name__ == "__main__":
     # board.print_thresh_board()
     # print("contours")
     # board.print_board_contours()
-    print("board")
-    board.print_board()
+    # print("board")
+    # board.print_board()
+    print("cells")
+    board.print_cells()

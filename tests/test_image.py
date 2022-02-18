cf4b9c42d4af5143f39d94a1be1ecd2a2fe8bf92
klawik-j <klawik.j@gmail.com>
2022-02-18 22:58:41 +0100

    Refactor: change const name to more meaningful

--- tests/test_image.py
+++ tests/test_image.py
@@ -4,18 +4,18 @@ import pytest
 
 from sudoku_ocr.image import Image
 
-CORRECT_FILE_PATH = Path("tests/img/sudoku1.png")
-INCORRECT_FILE_PATH = Path("incorrect/file/path")
+CORRECT_IMAGE_PATH = Path("tests/img/sudoku1.png")
+INCORRECT_IMAGE_PATH = Path("incorrect/file/path")
 
 
 def test_load_existing_file():
     """Test load existing file."""
-    image = Image(CORRECT_FILE_PATH)
-    assert image.path == CORRECT_FILE_PATH
+    image = Image(CORRECT_IMAGE_PATH)
+    assert image.path == CORRECT_IMAGE_PATH
     assert image.data.any()
 
 
 def test_load_non_existing_file():
     """Test load non existing file."""
     with pytest.raises(OSError):
-        Image(INCORRECT_FILE_PATH)
+        Image(INCORRECT_IMAGE_PATH)

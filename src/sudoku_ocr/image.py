d7e9f5a8d9d95c317bd41148382041d1d997d9b4
klawik-j <kklawik345@gmail.com>
2023-08-29 20:49:16 +0200

    Add: basic image class.

---
+++ src/sudoku_ocr/image.py
@@ -0,0 +1,107 @@
+"""Image properties."""
+
+from os.path import isfile
+from pathlib import Path
+from typing import Protocol
+
+from cv2 import (
+    ADAPTIVE_THRESH_GAUSSIAN_C,
+    COLOR_BGR2GRAY,
+    THRESH_BINARY,
+    GaussianBlur,
+    adaptiveThreshold,
+    bitwise_not,
+    cvtColor,
+    imread,
+    imshow,
+    waitKey,
+)
+from imutils import resize
+from numpy import ndarray
+
+WAIT_TIME = 2000
+
+
+class ImageAdapter(Protocol):
+    """Image adapter."""
+
+    def load_image(self, path: Path) -> None:
+        """Load image."""
+        ...
+
+    def resize(self) -> None:
+        """Resize image."""
+        ...
+
+    def thresholding(self) -> None:
+        """Implement thresholding on image."""
+        ...
+
+    @property
+    def data(self) -> ndarray:
+        """Image data property."""
+        ...
+
+    @data.setter
+    def data(self, data: ndarray) -> None:
+        ...
+
+    @property
+    def path(self) -> Path:
+        """Image path property."""
+        ...
+
+
+class Image(ImageAdapter):
+    """True Image class."""
+
+    def __init__(self) -> None:
+        """Initialize Image class."""
+        self._data: ndarray = None
+        self._path: Path = Path()
+
+    def load_image(self, path: Path) -> None:
+        """Load image from file."""
+        if not isfile(path):
+            raise FileNotFoundError(f"file {path} not found")
+        else:
+            self._data = imread(str(path))
+            self._path = path
+
+    def resize(self, width: int = 600) -> None:
+        """Resize image to desire width."""
+        self.data = resize(self.data, width=width)
+
+    def thresholding(self) -> None:
+        """Apply thresholding on image."""
+        grayscale = cvtColor(self.data, COLOR_BGR2GRAY)
+        blurred = GaussianBlur(grayscale, (7, 7), 3)
+        thresh = adaptiveThreshold(
+            blurred,
+            255,
+            ADAPTIVE_THRESH_GAUSSIAN_C,
+            THRESH_BINARY,
+            11,
+            2,
+        )
+        inverse = bitwise_not(thresh)
+        self.data = inverse
+
+    def show(self) -> None:
+        """Show visual representation of image on screen."""
+        imshow("x", self.data)
+        waitKey(WAIT_TIME)
+
+    @property
+    def data(self) -> ndarray:
+        """Image data property."""
+        return self._data
+
+    @data.setter
+    def data(self, data: ndarray) -> None:
+        self._data = data
+
+    @property
+    def path(self) -> Path:
+        """Image path property."""
+        return self._path
---
+++ tests/integration/test_image.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import pytest
+
+from sudoku_ocr.image import Image
+
+
+class TestImage:
+    def test_load_image_file_not_found(self) -> None:
+        image = Image()
+        with pytest.raises(FileNotFoundError):
+            image.load_image(Path("foo/path/file.py"))
+
+    def test_load_image_load_success(self) -> None:
+        image = Image()
+        image.load_image(Path("tests/img/no_sudoku2.jpg"))
+        assert image.data is not None
+        assert image.path is not None
+
+    def test_resize_image_default_value(self) -> None:
+        image = Image()
+        image.load_image(Path("tests/img/no_sudoku2.jpg"))
+        image.resize()
+        assert image.data.shape[1] == 600
+
+    def test_resize_image_custom_value(self) -> None:
+        image = Image()
+        image.load_image(Path("tests/img/no_sudoku2.jpg"))
+        image.resize(width=500)
+        assert image.data.shape[1] == 500
+
+    def test_thresholding(self) -> None:
+        image = Image()
+        image.load_image(Path("tests/img/sudoku1.png"))
+        image.resize(10)
+        image.thresholding()
+        proper_data = [
+            [255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 0, 0, 255, 255, 255, 255],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert (image.data == proper_data).all()
